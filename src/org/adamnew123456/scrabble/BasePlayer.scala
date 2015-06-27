cff6efb453f58bd5c23e4f5ce89ee6a3eaeca84c
adamnew123456 <adamnew123456@gmail.com>
2015-06-27 17:45:19 -0400

    Removed AbstractPlayer in favor of BasePlayer.
    I made this decision to simplify the interface for
    players, which jammed together things that the game
    coordinator should keep track of (like score), which
    the player shouldn't be able to freely modify.
    
    This also caused me to remove the tests for 
    AbstractPlayer, since everything checked by those tests
    is going to be moved to the game anyway.

---
+++ src/org/adamnew123456/scrabble/BasePlayer.scala
@@ -0,0 +1,52 @@
+package org.adamnew123456.scrabble
+
+import scala.collection.mutable.HashMap
+import scala.util.{Try, Success, Failure}
+
+/**
+ * An error raised when a player tries to use a tile that isn't in their hand.
+ */
+case class NoTileInHandError(tile: Char) extends Exception {
+  override def toString = s"NoTileInHandError($tile)"
+}
+
+/**
+ * This is mostly just a small interface for defining a player. It is only
+ * capable of doing three things:
+ * 
+ * - Reacting to the start and end of a turn.
+ * - Requesting or rejecting an offer to replace any tiles that the player
+ *   wants replaced.
+ * - Executing a turn with a given board and rack of tiles, returning a map
+ *   of characters to add to the board.
+ */
+trait BasePlayer {
+  /**
+   * This determines whether or not the game should replace some of the
+   * player's tiles with tiles drawn from the bag.
+   * 
+   * Note that any invalid requests will result in this function being
+   * called again, on the same turn. If there aren't any tiles to replace,
+   * then this function will not get called.
+   */
+  def replaceTiles(tiles: List[Char]): List[Char]
+  
+  /**
+   * This executes the turn, producing a map of the characters to add to the
+   * board.
+   * 
+   * Note that providing an invalid set of moves will result in this function
+   * being called again, on the same turn.
+   */
+  def turn(game: Config, board: Board, tiles: List[Char]): Map[(Int, Int), Char]
+  
+  /**
+   * This is executed at the beginning of a turn.
+   */
+  def startTurn(board: Board, tiles: List[Char], score: Int): Unit
+  
+  /**
+   * This is executed at the end of a turn.
+   */
+  def endTurn(board: Board, tiles: List[Char], score: Int): Unit
+}
