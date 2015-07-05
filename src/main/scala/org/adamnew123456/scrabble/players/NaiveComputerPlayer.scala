c249b5b519de0ff0e1fb27a8adcb4599663b5677
adamnew123456 <adamnew123456@gmail.com>
2015-07-05 11:33:32 -0400

    Wrote a computer player.

---
+++ src/main/scala/org/adamnew123456/scrabble/players/NaiveComputerPlayer.scala
@@ -0,0 +1,65 @@
+package org.adamnew123456.scrabble.players
+
+import scala.annotation.tailrec
+import scala.util.{Try, Success, Failure}
+
+import org.adamnew123456.scrabble.{BasePlayer, Board, Config, EndGame, TileGroup, WordScorer}
+
+/**
+ * This is a fairly simple computer player, which will simply try to find the
+ * best move it can within a certain timeframe, by plugging letters into
+ * various tiles and seeing if they score.
+ */
+class NaiveComputerPlayer(name: String, game: Config, scorer: WordScorer, 
+                          thinkTimeNano: Long = NaiveComputerPlayer.MAX_TIMEFRAME_NS)
+    extends BasePlayer(name, game, scorer) {
+  
+  def replaceTiles(tiles: TileGroup, maxReplace: Int): TileGroup = {
+    // This AI doesn't consider replacing tiles 
+    TileGroup.fromTraversable(Nil)
+  }
+  
+  def turn(board: Board, tiles: TileGroup): Map[(Int, Int), Char] = {
+    // First, find out which tiles can have tiles added to them
+    val squares = for {col <- 0.to(board.width - 1)
+                       row <- 0.to(board.height - 1)}
+                  yield {
+                    // Check all the adjacent tiles, and see if any of them are
+                    // occupied by a tile
+                    val left = (col - 1, row)
+                    val right = (col + 1, row)
+                    val above = (col, row - 1)
+                    val below = (col, row + 1)
+                    
+                    val adjacent = List(left, right, above, below).map(board.get)
+                    if (adjacent.exists(_.isDefined)) {
+                      Some((col, row))
+                    } else {
+                      None
+                    }
+                  }
+                  
+    val validSquares = squares.filter(_.isDefined).map(_.get)
+    
+    // These are the words on the board originally, for comparison
+    val oldWords = board.findWords.map(_.text)
+    
+    val moveGenerator = new NaiveMoveGenerator(board, tiles, scorer, validSquares)
+    val targetTime = System.nanoTime + thinkTimeNano
+    moveGenerator.run(targetTime) match {
+      case Some(moves) => moves
+      case None        => Map[(Int, Int), Char]()
+    }
+  }
+  
+  def startTurn(board: Board, tiles: TileGroup, score: Map[String, Int]) = ()
+  def endTurn(board: Board, tiles: TileGroup, score: Map[String, Int]) = ()
+  def endGame(result: EndGame) = ()
+}
+
+object NaiveComputerPlayer {
+  // How long the computer player has to find the best match it can - in
+  // nanoseconds, since we use System.nanoTime because it is (usually)
+  // monotonic
+  val MAX_TIMEFRAME_NS: Long = (30 * 1e9).toLong
+}
---
+++ src/main/scala/org/adamnew123456/scrabble/players/NaiveMoveGenerator.scala
@@ -0,0 +1,152 @@
+package org.adamnew123456.scrabble.players
+
+
+import scala.util.{Try, Success, Failure}
+import org.adamnew123456.scrabble.{Board, TileGroup, WordScorer}
+
+/**
+ * This generates the moves for the NaiveComputerPlayer - the procedure here is
+ * complex enough that it was worthwhile to extract it from the 
+ * NaiveComputerPlayer.
+ */
+class NaiveMoveGenerator(board: Board, tiles: TileGroup, scorer: WordScorer, openSpaces: IndexedSeq[(Int, Int)]) {
+  /**
+   * To define what moves should be possible for a turn, the following
+   * structure is going to be used:
+   * 
+   * - A Move(column, row, tile) is used to indicate that a tile is placed at 
+   *   the given location
+   * - An And(moveA, moveB) is used to combine a pair of moves, doing them both
+   *   in the same turn.
+   *   
+   * This makes it easy to combine multiple moves per turn, if that would be
+   * advantageous in terms of points.
+   */
+  trait Strategy
+  case class Move(column: Int, row: Int, tile: Char) extends Strategy
+  case class And(first: Strategy, second: Strategy) extends Strategy
+  
+  // Finds out what tiles would be used by a given strategy
+  def findTilesUsed(strategy: Strategy): TileGroup =
+    strategy match {
+      case Move(_, _, tile) => TileGroup.fromTraversable(List(tile))
+      case And(a, b)        => findTilesUsed(a).merge(findTilesUsed(b))
+    }
+  
+  // Finds the spaces used by a given strategy
+  def findSpacesUsed(strategy: Strategy): Set[(Int, Int)] =
+    strategy match {
+      case Move(col, row, _) => Set((col, row))
+      case And(a, b)         => findSpacesUsed(a) ++ findSpacesUsed(b)
+    }
+  
+  /*
+   * With strategies defined, an implementation strategy jumps out:
+   * 
+   * - Have one part generate a batch of move strategies, as necessary.
+   * - Have another part translate those strategies into moves.
+   * - Have a final part which takes those moves and either discards them, or
+   *   promotes them. Discarding can happen when the strategy isn't the best,
+   *   or it is invalid. Promotion happens when the strategy is the best 
+   *   strategy of all strategies seen so far.
+   */
+  
+  /**
+   * This combinatorially generates moves. It will either generate a fresh list
+   * of Move objects if not given a seed, or will add onto the given seeds
+   * by combining them with the And combinator.
+   */
+  def moveGenerator(seeds: List[Strategy] = Nil): Stream[Strategy] = {
+    if (seeds == Nil) {
+      // Simply take all the spaces, and all the tiles, and jam each possible
+      // tile into each possible space
+      val availableTiles = tiles.asList.toSet
+      val moves = for {
+        (col, row) <- openSpaces
+        tile <- availableTiles
+      } yield Move(col, row, tile)
+      
+      moves.toStream #::: moveGenerator(moves.toList)
+    } else {
+      // If we've got a previous seed, then start working combinatorially and
+      // generate moves
+      val moves = seeds.flatMap { oldMove: Strategy =>
+        val tilesUsed = findTilesUsed(oldMove)
+        val tilesLeft = tiles.remove(tilesUsed)
+        
+        // I *could* have squeezed this into a for comprehension, but it would
+        // have been messy expressing everything in terms of List
+        tilesLeft match {
+          case Success(freeTiles) =>
+            val uniqueFreeTiles = freeTiles.asList.toSet
+            uniqueFreeTiles.toList.flatMap { freeTile: Char =>
+              val spacesUsed = findSpacesUsed(oldMove)
+              val freeSpaces = openSpaces.toSet - spacesUsed
+              
+              freeSpaces.toList.map {
+                case (col: Int, row: Int) => And(oldMove, Move(col, row, freeTile))
+              }
+            }
+            
+          case Failure(_) =>
+            List()
+        }
+      }
+      
+      moves.toStream #::: moveGenerator(moves)
+    }
+  }
+  
+  // Converts a strategy into an actual move Map
+  def toMove(strategy: Strategy): Map[(Int, Int), Char] = 
+    strategy match {
+      case Move(col, row, tile) => Map((col, row) -> tile)
+      case And(a, b)            => toMove(a) ++ toMove(b)
+    }
+  
+  // This processes a move, returning either a Some of the move and the score, 
+  // or None if the move is invalid
+  def processMove(strategy: Strategy): Option[(Int, Map[(Int, Int), Char])] = {
+    val move = toMove(strategy)
+    val oldWords = board.findWords.map(_.text)
+    
+    val turnScore = for {
+      newBoard <- board.addCharacters(move)
+      newWords <- Success(newBoard.findWords.map(_.text))
+      
+      wordDiff <- Success(scorer.computeModifiedWords(oldWords, newWords))
+      score <- scorer.computeTurnScore(wordDiff)
+    } yield score
+    
+    turnScore match {
+      case Success(score) => Some(score, move)
+      case Failure(_)     => None
+    }
+  }
+  
+  // This runs down the stream of generated moves, and finds the best one while
+  // respecting the time limit we're given
+  def run(maxTime: Long): Option[Map[(Int, Int), Char]] = {
+    var bestMove: Option[Map[(Int, Int), Char]] = None
+    var bestMoveScore = 0
+    val strategies = moveGenerator()
+    strategies.foreach { strategy: Strategy =>
+      val time = System.nanoTime
+      if (time >= maxTime) {
+        return bestMove
+      } else {
+        processMove(strategy) match {
+          // Only record a score that is the best score seen so far
+          case Some((score, move)) =>
+            if (score > bestMoveScore) {
+              bestMove = Some(move)
+              bestMoveScore = score
+            }
+          case None => ()
+        }
+      }
+    }
+    
+    bestMove
+  }
+}
