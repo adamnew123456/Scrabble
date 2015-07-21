b1d7bba0fe21ad7e936180d872d1160021904218
adamnew123456 <adamnew123456@gmail.com>
2015-07-21 13:53:12 -0400

    Wrote the BoardView.
    The BoardView is responsible for displaying the tiles on the game board,
    and taking input (in the form of clicks) to manipulate it. I also added a
    small script to test the BoardView and ensure that it works.

---
+++ board-view-test.sc
@@ -0,0 +1,135 @@
+import java.awt._
+import java.awt.event._
+import javax.swing._
+import scala.collection.mutable.HashMap
+import scala.util.{ Try, Success, Failure }
+
+import org.adamnew123456.scrabble._
+import org.adamnew123456.scrabble.players.swing._
+
+class RunClosure(closure: () => Unit) extends Runnable {
+  def run = closure()
+}
+
+class ActionClosure(closure: ActionEvent => Unit) extends ActionListener {
+  def actionPerformed(event: ActionEvent) = closure(event)
+}
+
+class SelectionView(selection: TileSelection) extends JLabel("No tile selected") {
+  selection.attachObserver { tile: Option[Char] =>
+    println(s"Selection changed: $tile")
+    tile match {
+      case Some(tile) => setText("Selected: " + tile)
+      case None       => setText("No tile selected")
+    }
+  }
+}
+
+class SelectionEdit(selection: TileSelection) extends JPanel {
+  setLayout(new BoxLayout(this, BoxLayout.Y_AXIS))
+
+  val input = new JTextField("")
+  val change = new JButton("Change Selection")
+  change.addActionListener(new ActionClosure({ _ =>
+    val newSelection = input.getText
+    if (newSelection != "") {
+      println(s"Setting selection: ${newSelection(0)}")
+      selection.set(newSelection(0))
+    } else {
+      println("Clearing selection")
+      selection.clear
+    }
+  }))
+
+  add(input)
+  add(change)
+}
+
+/**
+ * This is like the default TurnBuilder, but it doesn't change the
+ * tile rack. This is because the RackView isn't included in this test, and
+ * I want to avoid complicating it by adding it. 
+ *
+ * It also omits a lot of checks that the original TurnBuilder does, so it may
+ * well break. However, this isn't what's under test here, so that doesn't
+ * matter.
+ */
+class TestTurnBuilder extends BaseObservable[ObservableTurnBuilder] with MutableTurnBuilder with ObservableTurnBuilder {
+  val stockBoardTiles = Map((7, 7) -> 'c', (7, 8) -> 'a', (7, 9) -> 't')
+  val stockBoard = Board.empty(15, 15).addCharacters(stockBoardTiles) match {
+    case Success(board) => board
+    case Failure(exn) =>
+      println(s"ERROR ON LINE 62: $exn")
+      ???
+  }
+  val stockTiles = TileGroup.fromTraversable("abcdefg")
+
+  val additions = HashMap[(Int, Int), Char]()
+  def getBoard = stockBoard.addCharacters(additions.toMap) match {
+    case Success(board) => board
+    case Failure(exn) =>
+      println(s"ERROR ON LINE 70: $exn")
+      ???
+  }
+
+  def getTiles = stockTiles
+  def getAdditions = additions.toMap
+
+  def addTiles(tiles: Map[(Int, Int), Char]): Try[Unit] = {
+    val result = for {
+      tempBoard <- stockBoard.addCharacters(additions.toMap)
+      result <- tempBoard.addCharacters(tiles)
+    } yield result
+
+    result match {
+      case Success(_) =>
+        additions ++= tiles
+        notifyObservers(this)
+        Success(())
+      case Failure(exn) =>
+        Failure(exn)
+    }
+  }
+
+  def addWord(word: String, location: (Int, Int), direction: Direction.Type): Try[Unit] = 
+    ???
+
+  def removeTiles(spaces: Set[(Int, Int)]) = {
+    additions --= spaces
+    notifyObservers(this)
+    Success(())
+  }
+
+  def reload(board: Board, rack: TileGroup) =
+    additions.clear
+}
+
+SwingUtilities.invokeLater(new RunClosure({ () =>
+  val main = new JFrame("BoardView Test")
+  main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
+  main.getContentPane.setLayout(new BorderLayout())
+
+  val selection = new TileSelection()
+  val turnBuilder = new TestTurnBuilder()
+  val errorReporter = new ErrorReporter()
+
+  val selectionView = new SelectionView(selection)
+  val selectionEdit = new SelectionEdit(selection)
+  val errorView = new SwingErrorReporter(5000, errorReporter)
+
+  val config = new FileConfig("bin/letter-dist.txt", "bin/letter-score.txt", "bin/words.txt")
+  val boardView = new BoardView(config, selection, errorReporter, turnBuilder)
+
+  turnBuilder.attachObserver(boardView.builderObserver)
+
+  // Get the initial part of the board set up
+  boardView.builderObserver(turnBuilder)
+
+  main.getContentPane.add(errorView, BorderLayout.SOUTH)
+  main.getContentPane.add(selectionView, BorderLayout.NORTH)
+  main.getContentPane.add(selectionEdit, BorderLayout.EAST)
+  main.getContentPane.add(boardView, BorderLayout.CENTER)
+
+  main.pack()
+  main.setVisible(true)
+}))
---
+++ src/main/scala/org/adamnew123456/scrabble/players/swing/BoardView.scala
@@ -0,0 +1,180 @@
+package org.adamnew123456.scrabble.players.swing
+
+import java.awt.GridLayout
+import java.awt.event.MouseEvent
+import javax.swing.JPanel
+import scala.collection.mutable.{ HashMap, HashSet } 
+import scala.util.{ Try, Success, Failure }
+
+import org.adamnew123456.scrabble._
+
+/**
+ * The BoardView is responsible for visualizing the contents of the game board,
+ * and allowing the player to move tiles on and off the game board by clicking
+ * on it.
+ */
+class BoardView(config: Config, selection: TileSelection, reporter: ErrorReporter, builder: MutableTurnBuilder) extends JPanel {
+  // Keep a hold on the GridLayout, since we need to resize it later when we
+  // get an actual Board
+  val gridLayout = new GridLayout(1, 1)
+  setLayout(gridLayout)
+  
+  private val tiles = HashMap[(Int, Int), JPanel]()
+  
+  // Start out with a blank widget, and fill it in as we get notifications
+  tiles((0, 0)) = new EmptyTileView()
+  updateContainer(1, 1)
+  
+  /**
+   * Adds all the tiles to this widget, in the proper location on the grid, and
+   * makes them visible.
+   */
+  private def updateContainer(width: Int, height: Int) {
+    for (col <- 0.to(width - 1) ;
+         row <- 0.to(height - 1)) {
+      val component = tiles((col, row))
+      add(component)
+    }
+    
+    revalidate
+  }
+  
+  /**
+   * Creates a new active tile of the given letter, and at the given location.
+   */
+  private def makeActiveTile(col: Int, row: Int, tile: Char): TileView = {
+    val score = config.letterScores(tile)
+    val tileView = new TileView(tile, score, Active)
+    val handler = new ClosureButtonListener({ event: MouseEvent =>
+      (event.getID, event.getButton) match {
+        case (MouseEvent.MOUSE_CLICKED, MouseEvent.BUTTON1) =>
+          selection.clear
+          
+        case (MouseEvent.MOUSE_CLICKED, MouseEvent.BUTTON3) =>
+          builder.removeTiles(Set((col, row))) match {
+            case Failure(err) => reporter.report(err)
+            case _            => ()
+          }
+        case _ => ()
+      }
+    })
+    
+    tileView.addMouseListener(handler)
+    tileView
+  }
+  
+  /**
+   * Creates a new permanent tile of the given letter, and at the given location.
+   */
+  private def makePermanentTile(col: Int, row: Int, tile: Char): TileView = {
+    val score = config.letterScores(tile)
+    val tileView = new TileView(tile, score, Inactive)
+    val handler = new ClosureButtonListener({ event: MouseEvent =>
+      (event.getID, event.getButton) match {
+        case (MouseEvent.MOUSE_CLICKED, MouseEvent.BUTTON1) =>
+          selection.clear
+          
+        case _ => ()
+      }
+    })
+    
+    tileView.addMouseListener(handler)
+    tileView
+  }
+  
+  /**
+   * Creates a new blank tile at the given location.
+   */
+  private def makeEmptyTile(col: Int, row: Int): EmptyTileView = {
+    val emptyTile = new EmptyTileView()
+    val handler = new ClosureButtonListener({ event: MouseEvent =>
+      (event.getID, event.getButton) match {
+        case (MouseEvent.MOUSE_CLICKED, MouseEvent.BUTTON1) =>
+          if (selection.hasSelection) {
+            val selected = selection.get
+            selection.clear
+            
+            builder.addTiles(Map((col, row) -> selected)) match {
+              case Failure(err) => reporter.report(err)
+              case _            => ()
+            }
+          }
+        case _ => ()
+      }
+    })
+    
+    emptyTile.addMouseListener(handler)
+    emptyTile
+  }
+  
+  /**
+   * This hooks into TurnBuilder, and updates the contents of the board when
+   * it changes.
+   */
+  val builderObserver = {
+    observableBuilder: ObservableTurnBuilder =>
+      // Remove old widgets, both from the tiles map, but also the widget 
+      // hierarchy
+      tiles.values.map(remove(_))
+      tiles.clear
+      
+      val board = observableBuilder.getBoard
+      val additions = observableBuilder.getAdditions
+      
+      // This has to be updated, since the default is 1x1 and no board would 
+      // be that small
+      gridLayout.setColumns(board.width)
+      gridLayout.setRows(board.height)
+      
+      // There are three kinds of spaces on the board:
+      // - Unoccupied spaces
+      // - Spaces which are occupied because of previous moves. Players cannot
+      //   change the tiles on these spaces.
+      // - Spaces which are occupied because of the current move. Players can
+      //   change the tiles on these spaces.
+      val blanks = HashSet[(Int, Int)]()
+      val permanent = HashMap[(Int, Int), Char]()
+      val turn = HashMap[(Int, Int), Char]()
+      
+      for (col <- 0.to(board.width - 1) ; row <- 0.to(board.height - 1)) {
+        if (additions.contains((col, row))) {
+          turn((col, row)) = additions((col, row))
+        } else if (board(col, row).isDefined) {
+          permanent((col, row)) = board(col, row).get
+        } else {
+          blanks += ((col, row))
+        }
+      }
+      
+      /*
+       * Blanks can be left-clicked, at which point the selected tile will be
+       * placed on the board at that location (if there is a selected tile).
+       */
+      blanks.map {
+        case (col, row) =>
+          val emptyTileView = makeEmptyTile(col, row)
+          tiles((col, row)) = emptyTileView
+      }
+      
+      /*
+       * Permanent tiles can be left-clicked, which clears the selection.
+       */
+      permanent.map {
+        case ((col, row), tile) =>
+          val tileView = makePermanentTile(col, row, tile)
+          tiles((col, row)) = tileView
+      }
+      
+      /*
+       * Tiles added during the turn can be left-clicked, which clears the
+       * selection, or right-clicked, which removes them from the board.
+       */
+      turn.map {
+        case ((col, row), tile) =>
+          val tileView = makeActiveTile(col, row, tile)
+          tiles((col, row)) = tileView
+      }
+      
+      updateContainer(board.width, board.height)
+  }
+}
