e73a83153cbfea00ab33f5f16b132fe7a4dc72ce
adamnew123456 <adamnew123456@gmail.com>
2015-07-25 10:45:22 -0400

    Added a MultiTileSelection, for selecting groups of tiles.
    This will be useful for the rack, which has to have multiple-selections
    when the player chooses which tiles to replace.

--- src/main/scala/org/adamnew123456/scrabble/players/swing/TileSelection.scala
+++ src/main/scala/org/adamnew123456/scrabble/players/swing/TileSelection.scala
@@ -1,8 +1,8 @@
 package org.adamnew123456.scrabble.players.swing
 
-import scala.collection.mutable.HashSet
+import scala.collection.mutable.HashMap
 
-import org.adamnew123456.scrabble.BaseObservable
+import org.adamnew123456.scrabble.{ BaseObservable, TileGroup }
 
 /**
  * The TileSelection is used to communicate, between two components, that a 
@@ -29,15 +29,70 @@ class TileSelection extends BaseObservable[Option[Char]] {
   /**
    * Sets the current selection.
    */
-  def set(tile: Char) = {
-    selection = Some(tile)
-    notifyObservers(Some(tile))
-  }  
+  def set(tile: Char) =
+    selection match {
+      case Some(`tile`) => ()
+      case _ =>
+        selection = Some(tile)
+        notifyObservers(selection)
+    }  
+  
   /**
    * Clears the current selection.
    */
   def clear = {
-    selection = None
-    notifyObservers(None)
+    if (selection.isDefined) {
+      selection = None
+      notifyObservers(selection)
+    }
+  }
+}
+
+/**
+ * The MultiTileSelection is used to communicate that a group of tiles is
+ * selected between two components.
+ */
+class MultiTileSelection extends BaseObservable[TileGroup] {
+  val selection = HashMap[Char, Int]()
+  
+  /**
+   * Gets the current selection as a TileGroup.
+   */
+  def get = TileGroup.fromMap(selection.toMap)
+  
+  /**
+   * Adds a tile to the current selection.
+   */
+  def +=(tile: Char) = {
+    if (selection.contains(tile)) {
+      selection(tile) += 1
+    } else {
+      selection(tile) = 1
+    }
+    
+    notifyObservers(get)
+  }
+  
+  /**
+   * Removes a tile from the current selection. Returns true if the tile was
+   * removed, or false if the tile was not in the selection to begin with.
+   */
+  def -=(tile: Char) =
+    if (selection.contains(tile) && selection(tile) > 0) {
+      selection(tile) -= 1
+      notifyObservers(get)
+      true
+    } else {
+      false
+    }
+  
+  /**
+   * Clears the current selection.
+   */
+  def clear = {
+    if (!selection.isEmpty) {
+      selection.clear
+      notifyObservers(get)
+    }
   }
 }
--- src/test/scala/org/adamnew123456/scrabble/test/TileSelectionTest.scala
+++ src/test/scala/org/adamnew123456/scrabble/test/TileSelectionTest.scala
@@ -1,6 +1,7 @@
 package org.adamnew123456.scrabble.test
 
-import org.adamnew123456.scrabble.players.swing.TileSelection
+import org.adamnew123456.scrabble.TileGroup
+import org.adamnew123456.scrabble.players.swing.{ MultiTileSelection, TileSelection }
 
 import org.junit.Test
 import junit.framework.TestCase
@@ -38,15 +39,109 @@ class TileSelectionTest extends TestCase {
     assertEquals(selection.get, 'a')
     
     assertTrue(observer.wasNotified)
-    assertEquals(observer.notifyValue, Some('a'))
+    assertEquals(observer.notifyValue, selection.maybeGet)
+    
+    observer.wasNotified = false
+    // Ensure that the observers are only notified if the selection changes
+    selection.set('a')
+    assertFalse(observer.wasNotified)
   }
   
   def testClearSelection {
+    // Prime the selection, so that the first clear isn't skipped
+    selection.set('a')
+    observer.wasNotified = false
+    
     selection.clear
     assertFalse(selection.hasSelection)
     assertEquals(selection.maybeGet, None)
     
     assertTrue(observer.wasNotified)
-    assertEquals(observer.notifyValue, None)
+    assertEquals(observer.notifyValue, selection.maybeGet)
+    
+    observer.wasNotified = false
+    // Clearing the rack a second time should not set off the observers
+    selection.clear
+    assertFalse(observer.wasNotified)
+  }
+}
+
+/**
+ * This ensures that the MultiTileSelection works as intended.
+ */
+class MultiTileSelectionTest extends TestCase {
+  var selection: MultiTileSelection = _
+  
+  class SelectionObserver {
+    var wasNotified = false
+    var notifyValue: TileGroup = _
+    
+    val observe = { tiles: TileGroup =>
+      notifyValue = tiles
+      wasNotified = true
+    }
+  }
+  
+  var observer: SelectionObserver = _
+  
+  override def setUp {
+    selection = new MultiTileSelection()
+    observer = new SelectionObserver()
+    
+    selection.attachObserver(observer.observe)
+  }
+  
+  def testAddTiles() {
+    // Test adding a new value that wasn't in the selection before
+    selection += 'a'
+    assertTrue(observer.wasNotified)
+    assertEquals(observer.notifyValue, TileGroup.fromTraversable("a"))
+    assertEquals(selection.get, observer.notifyValue)
+    
+    observer.wasNotified = false
+    // Test adding a new value that was already in the selection
+    selection += 'a'
+    assertTrue(observer.wasNotified)
+    assertEquals(observer.notifyValue, TileGroup.fromTraversable("aa"))
+    assertEquals(selection.get, observer.notifyValue)
+  }
+  
+  def testRemoveTiles() {
+    selection += 'a'
+    selection += 'a'
+    observer.wasNotified = false
+    
+    // Test removing one of >1 tiles of the same kind
+    selection -= 'a'
+    assertTrue(observer.wasNotified)
+    assertEquals(observer.notifyValue, TileGroup.fromTraversable("a"))
+    assertEquals(selection.get, observer.notifyValue)
+    
+    observer.wasNotified = false
+    // Test removing the only tile of a kind
+    selection -= 'a'
+    assertTrue(observer.wasNotified)
+    assertEquals(observer.notifyValue, TileGroup.fromTraversable(""))
+    assertEquals(selection.get, observer.notifyValue)
+    
+    observer.wasNotified = false
+    // Finally, ensure that removing a non-existent tile does nothing
+    selection -= 'a'
+    assertFalse(observer.wasNotified)
+    assertEquals(selection.get, TileGroup.fromTraversable(""))
+  }
+  
+  def testClearTiles() {
+    selection += 'a'
+    selection += 'a'
+    observer.wasNotified = false
+    
+    // Clearing the rack should remove the tiles on it
+    selection.clear
+    assertTrue(observer.wasNotified)
+    assertEquals(observer.notifyValue, TileGroup.fromTraversable(""))
+    assertEquals(selection.get, observer.notifyValue)
+    
+    // Clearing an empty rack should not trigger the observers
   }
 }
